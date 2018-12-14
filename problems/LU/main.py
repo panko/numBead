a301581e41d87bf80e7a38e2403f4119a3e7a185
prike <prike18@gmail.com>
2018-12-14 01:12:14 +0100

    LU decompisition works +10p wohoo

--- problems/LU/main.py
+++ problems/LU/main.py
@@ -1,75 +1,37 @@
+#!/usr/bin/env python3
 
-
-def read_input():
+def readInput():
     li = []
     n = int(input())
     for i in range(n):
         li.append(list(map(float, input().strip().split(' '))))
     return li
 
-def mult_matrix(A,B):
-    R = [[0.0 for i in range(len(B[0]))] for j in range(len(A))]
-    for i in range(len(A)):
-        for j in range(len(B[0])):
-            for k in range(len(A[0])):
-                R[i][j] += A[i][k] * B[k][j]
+def print_matrix(M):
+    """Pretty prints matrix"""
 
-    return R 
+    for i in range(len(M)):  
+        for j in range(len(M[0])):  
+            print("{:.12f}".format(M[i][j]), end=" ")
+        print("\n",end="") 
 
-def pivot_matrix(M):
-    """Returns the pivoting matrix for M, used in Doolittle's method."""
-    m = len(M)
-
-    # Create an identity matrix, with floating point values                                                                                                                                                                                            
-    id_mat = [[float(i ==j) for i in range(m)] for j in range(m)]
-
-    # Rearrange the identity matrix such that the largest element of                                                                                                                                                                                   
-    # each column of M is placed on the diagonal of of M                                                                                                                                                                                               
-    for j in range(m):
-        row = max(range(j, m), key=lambda i: abs(M[i][j]))
-        if j != row:
-            # Swap the rows                                                                                                                                                                                                                            
-            id_mat[j], id_mat[row] = id_mat[row], id_mat[j]
-
-    return id_mat
-
-def lu_decomposition(A):
-    """Performs an LU Decomposition of A (which must be square)                                                                                                                                                                                        
-    into PA = LU. The function returns P, L and U."""
-    n = len(A)
-
-    # Create zero matrices for L and U                                                                                                                                                                                                                 
-    L = [[0.0] * n for i in range(n)]
-    U = [[0.0] * n for i in range(n)]
-
-    # Create the pivot matrix P and the multipled matrix PA                                                                                                                                                                                            
-    P = pivot_matrix(A)
-    PA = mult_matrix(P, A)
-
-    # Perform the LU Decomposition                                                                                                                                                                                                                     
-    for j in range(n):
-        # All diagonal entries of L are set to unity                                                                                                                                                                                                   
-        L[j][j] = 1.0
-
-        # LaTeX: u_{ij} = a_{ij} - \sum_{k=1}^{i-1} u_{kj} l_{ik}                                                                                                                                                                                      
-        for i in range(j+1):
-            s1 = sum(U[k][j] * L[i][k] for k in range(i))
-            U[i][j] = PA[i][j] - s1
-
-        # LaTeX: l_{ij} = \frac{1}{u_{jj}} (a_{ij} - \sum_{k=1}^{j-1} u_{kj} l_{ik} )                                                                                                                                                                  
-        for i in range(j, n):
-            s2 = sum(U[k][j] * L[i][k] for k in range(j))
-            L[i][j] = (PA[i][j] - s2) / U[j][j]
-
-
-    return (P, L, U)
-
+def lu_decomposition(M):
+    for i in range(len(M)):  
+        for j in range(i+1, len(M)):
+            Uresult = (M[j][i] / M[i][i])*-1
+            for k in range(i, len(M)):
+                M[j][k] = M[j][k] + (Uresult*M[i][k])
+            M[j][i] = Uresult*-1
+    return M
 
 def main():
-    li = read_input()
-    P,L,U = lu_decomposition(li)
-    print(L)
-    print(U)
-
+    matrix = readInput()
+    try:
+        M = lu_decomposition(matrix)
+    except ZeroDivisionError as e:
+        print("fail")
+    else:
+        print_matrix(M)
+    
 if __name__ == '__main__':
     main()
